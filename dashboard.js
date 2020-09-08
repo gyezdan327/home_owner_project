3c8441889d09bb5394eefb9fd9878279eee40deb
gyezdan327 <60323996+gyezdan327@users.noreply.github.com>
2020-09-08 16:27:29 -0400

    Frontend<>Backend initial integration

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,15 @@
-var temperature = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76];
-var humidity = [28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64];
+function getrandTemp(){
+	var temperature = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76];
+	var randInt=getRndInteger(1,13)
+	randTemp = temperature[randInt];	
+	document.write(randTemp);
+}
 
-var randTemp = Array(6);
-var randHumidity = Array(6);
-
-//Generate randTemp and randHumidity that includes 6 random temp&humidity values
-for (i=0; i<6; i++) {
-  var randInt = getRndInteger(0,13);
-  randTemp[i] = "Temperature: " + temperature[randInt].toString() + "°F";
-  randHumidity[i] = "Humidity: " + humidity[randInt].toString() + "%";
+function getrandHumidity (){
+	var humidity = [28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64];
+	var randInt=getRndInteger(1,13)
+	randHumidity = humidity[randInt];
+	document.write(randHumidity);
 }
 
 function getRndInteger(min, max) {
