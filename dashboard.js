ca2fe6f5cf76751365aa2e1b9790d9cfb5f0b684
Kaito Yamagishi <kaitoyamagishi1109@gmail.com>
2020-09-09 12:58:03 -0400

    rand. temp and humidity funcs edited
    Made sure the temperature and humidity arrays' indexes mached (temp&humidity combination makes more sense)

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,11 @@
-function getrandTemp(){
+function getrandTemp(randInt){
 	var temperature = [64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76];
-	var randInt=getRndInteger(1,13)
 	randTemp = temperature[randInt];	
 	document.write(randTemp);
 }
 
-function getrandHumidity (){
+function getrandHumidity (randInt){
 	var humidity = [28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64];
-	var randInt=getRndInteger(1,13)
 	randHumidity = humidity[randInt];
 	document.write(randHumidity);
 }
