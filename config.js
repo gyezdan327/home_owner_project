ce23814fb0be78c6b4bb6a13313590dad7466254
gyezdan327 <60323996+gyezdan327@users.noreply.github.com>
2020-09-17 17:34:18 -0400

    added config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var fireBaseConfig = {
+//Insert firbase configuration here
+};
+
+var weatherApiConfig = {
+    key: "//Insert Accuweather API Key here"
+};
+
+var newsApiConfig = {
+	key: "//Insert New York Times most Popular News API Key here"
+};
